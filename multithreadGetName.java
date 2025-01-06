2b4e8c19debaab610d02aac61a9eae25dfa4bdaa
Sahil Singh <sahilsingh18012002@gmail.com>
2025-01-07 00:51:49 +0530

    multithreadGetName.java

---
+++ multithreadGetName.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(Thread.currentThread().getName());
+        // getting name of that thread , here its shows (main) as it is main thread for the program.
+    }
+}
