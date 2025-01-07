644b197e2b47caafd2b4695808cbc97f4c598149
Sahil Singh <sahilsingh18012002@gmail.com>
2025-01-07 13:14:45 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+public class test {
+        public static void main(String[] args) {
+
+        World one = new World();
+        one.start();
+
+        for (; ; ) {
+            System.out.println("winner");
+        }
+//            System.out.println(Thread.currentThread().getName());
+    }
+}
+
+//main thread
