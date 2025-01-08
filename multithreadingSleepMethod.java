8007eecb0ca3e41a78ee2818bda44925b53d7d0a
Sahil Singh <sahilsingh18012002@gmail.com>
2025-01-08 12:05:46 +0530

    Add files via upload

---
+++ multithreadingSleepMethod.java
@@ -0,0 +1,22 @@
+public class multithreadingSleepMethod extends Thread{
+    @Override
+
+    public void run(){
+        for(int i =  1; i<5 ; i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        multithreading t1 = new multithreading();
+        t1.start();
+
+    }
+}
