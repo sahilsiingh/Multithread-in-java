7185f834840f717b8ddece9eba15a18b7b0b30cc
Sahil Singh <sahilsingh18012002@gmail.com>
2025-01-09 13:26:28 +0530

    Create sychronizedThreads.java

---
+++ sychronizedThreads.java
@@ -0,0 +1,41 @@
+class Counter {
+    int count = 0;
+
+    // Synchronize the increment method to ensure thread safety
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        // Create two threads that will increment the counter
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        // Wait for both threads to finish
+        t1.join();
+        t2.join();
+
+        // Print the final count
+        System.out.println("Final count: " + counter.getCount());
+    }
+}
